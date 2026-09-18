d4c15bc8d9904fa35fcc68d447311bbc71f1c708
Wilder Taype <i201418741@cibertec.edu.pe>
2026-09-18 12:48:27 -0500

    Actualizacion Principal v3.10.10

--- src/wii.js
+++ src/wii.js
@@ -9,13 +9,13 @@ export const versionName = '3.0.0';
 export default { id, app, by, linkweb, linkme, version, versionName };
 
 /** ACTUALIZAR AL TAG POR SEGURIDAD [TAG NUEVO] (1)
-git tag v2 -m "Version v2" ; git push origin v2
+git tag v3 -m "Version v3" ; git push origin v3
 
 ACTUALIZACIÓN AL MAIN PRINCIPAL DEL PROYECTO [MAIN] (2)
 git add . ; git commit -m "Actualizacion Principal v3.10.10" ; git push origin main
 
 // REEMPLAZAR TAG DE SEGURIDAD EXISTENTE [TAG REMPLAZO] (3)
-git tag -d v2 ; git tag v2 -m "Version v2 actualizada" ; git push origin v2 --force
+git tag -d v3 ; git tag v3 -m "Version v3 actualizada" ; git push origin v3 --force
 
 // Actualizar versiones de seguridad [ELIMINAR CARPETA - ARCHIVO ONLINE] (5)
 git rm --cached skills-lock.json ; git commit -m "Archivo Eliminado" ; git push origin main
