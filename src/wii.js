d517db628de75b3908a0c5ae20bd1cfef71fde5e
Wilder Taype <i201418741@cibertec.edu.pe>
2026-09-18 12:48:08 -0500

    Actualizacion Principal v3.10.10

--- src/wii.js
+++ src/wii.js
@@ -4,15 +4,15 @@ export const app = 'Solgas Surquillo';
 export const by = '@wilder.taype';
 export const linkweb = 'https://gaswii.lourdesinformatica10.workers.dev/';
 export const linkme = 'https://wtaype.github.io/';
-export const version = 2.0;
-export const versionName = '2.0.0';
+export const version = 3.0;
+export const versionName = '3.0.0';
 export default { id, app, by, linkweb, linkme, version, versionName };
 
 /** ACTUALIZAR AL TAG POR SEGURIDAD [TAG NUEVO] (1)
 git tag v2 -m "Version v2" ; git push origin v2
 
 ACTUALIZACIÓN AL MAIN PRINCIPAL DEL PROYECTO [MAIN] (2)
-git add . ; git commit -m "Actualizacion Principal v2.10.10" ; git push origin main
+git add . ; git commit -m "Actualizacion Principal v3.10.10" ; git push origin main
 
 // REEMPLAZAR TAG DE SEGURIDAD EXISTENTE [TAG REMPLAZO] (3)
 git tag -d v2 ; git tag v2 -m "Version v2 actualizada" ; git push origin v2 --force
