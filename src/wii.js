9b5caf93d2d8db0cb828ab9148598b7463efae83
Wilder Taype <i201418741@cibertec.edu.pe>
2026-09-25 02:24:58 -0500

    Actualizacion Principal v4.10.10

--- src/wii.js
+++ src/wii.js
@@ -4,18 +4,18 @@ export const app = 'Solgas Surquillo';
 export const by = '@wilder.taype';
 export const linkweb = 'https://solgassurquillo.com/';
 export const linkme = 'https://wtaype.github.io/';
-export const version = 3.0;
-export const versionName = 'v2';
+export const version = 4.0;
+export const versionName = 'v4';
 export default { id, app, by, linkweb, linkme, version, versionName };
 
 /** ACTUALIZAR AL TAG POR SEGURIDAD [TAG NUEVO] (1)
-git tag v2 -m "Version v2" ; git push origin v2
+git tag v4 -m "Version v4" ; git push origin v4
 
 ACTUALIZACIÓN AL MAIN PRINCIPAL DEL PROYECTO [MAIN] (2)
-git add . ; git commit -m "Actualizacion Principal v2.10.10" ; git push origin main
+git add . ; git commit -m "Actualizacion Principal v4.10.10" ; git push origin main
 
 // REEMPLAZAR TAG DE SEGURIDAD EXISTENTE [TAG REMPLAZO] (3)
-git tag -d v2 ; git tag v2 -m "Version v3 actualizada" ; git push origin v3 --force
+git tag -d v4 ; git tag v4 -m "Version v3 actualizada" ; git push origin v3 --force
 
 // Actualizar versiones de seguridad [ELIMINAR CARPETA - ARCHIVO ONLINE] (5)
 git rm --cached skills-lock.json ; git commit -m "Archivo Eliminado" ; git push origin main
